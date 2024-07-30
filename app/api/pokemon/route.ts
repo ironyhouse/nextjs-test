4c94687817b3c8b4d9e2498221ca9f00a6f99558
Artsiom <36641168+ironyhouse@users.noreply.github.com>
2024-07-30 17:29:43 +0200

    fix: pokemon route updates

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -1,18 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import { Pokemon } from '@/app/types';
 
+// The following code is provided as an example of an alternative data fetching method in Next.js
+// https://nextjs.org/docs/app/building-your-application/routing/route-handlers#examples
 export async function GET(request: NextRequest) {
   const name = request.nextUrl.searchParams.get('name');
 
   if (!name) {
     console.error('Name was not provided.');
-    return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+    return Response.json({ error: 'Name is required' }, { status: 400 });
   }
 
   try {
     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+      // need to disable cache in get method
+      // https://nextjs.org/docs/app/building-your-application/routing/route-handlers#behavior
       cache: 'no-store',
-      method: 'GET',
     });
 
     if (!resp.ok) {
@@ -23,12 +26,13 @@ export async function GET(request: NextRequest) {
 
     const data: Pokemon = await resp.json();
 
-    return NextResponse.json(data, { status: 200 });
+    return Response.json(data, { status: 200 });
   } catch (error) {
     const errorMessage =
-      (error as Error)?.message || 'Failed to fetch Pokemon data!';
+      (error as Error)?.message || 'Failed to fetch pokemon data!';
+
     console.error('Error fetching Pokemon data:', error);
 
-    return NextResponse.json({ message: errorMessage }, { status: 500 });
+    return Response.json({ message: errorMessage }, { status: 500 });
   }
 }
