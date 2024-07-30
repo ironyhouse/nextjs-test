fc83cf104e82b46ebdd5915cf5feda030b1413ff
Artsiom <36641168+ironyhouse@users.noreply.github.com>
2024-07-30 16:17:24 +0200

    feat: added simple error handling

---
+++ app/csr/error.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { useRouter } from "next/navigation";
+
+export default function Error({
+	error,
+	reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+})
+{
+	const router = useRouter();
+
+	console.error("ERROR: An error occurred in CSR:", error);
+
+	return (
+		<div className="w-f h-full flex flex-col items-center justify-top p-4">
+			<h2>Something went wrong! </h2>
+			<button className="mt-4" onClick={() => reset()}>Try again</button>
+			<button onClick={() => router.replace('/')} >Home</button>
+		</div>
+	)
+}
