aaa2fc6fea73e3c9554e12f4eeb583c14b744aba
Artsiom <36641168+ironyhouse@users.noreply.github.com>
2024-07-30 19:28:31 +0200

    feat: added cypress

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+require('dotenv').config()
+
+export default defineConfig({
+	defaultCommandTimeout: 5000,
+	viewportWidth: 1920,
+	viewportHeight: 1200,
+	env: {},
+	e2e: {
+		baseUrl: 'http://localhost:3000',
+	},
+});
